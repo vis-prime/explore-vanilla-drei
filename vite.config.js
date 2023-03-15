c52cc49fcd5820c34f404271e036cd813ea8212e
vis-prime <vishals1030@gmail.com>
2023-03-15 22:59:17 +0530

    package updates

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import viteCompression from 'vite-plugin-compression'
 import gltf from 'vite-plugin-gltf'
-import { prune, dedup, textureCompress, draco } from '@gltf-transform/functions'
-import sharp from 'sharp'
 
 // https://vitejs.dev/config/
-const resolution = 1024
 
 export default defineConfig({
   build: {
@@ -16,26 +13,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    // gltf({
-    //   transforms: [
-    //     // remove unused resources
-    //     prune(),
-
-    //     // combine duplicated resources
-    //     dedup(),
-
-    //     textureCompress({
-    //       targetFormat: "avif",
-    //       encoder: sharp,
-    //       resize: [resolution, resolution],
-    //     }),
-
-    //     // compress mesh geometry
-    //     draco(),
-    //   ],
-    // }),
-    gltf(),
-    viteCompression(),
-  ],
+  plugins: [gltf(), viteCompression()],
 })
